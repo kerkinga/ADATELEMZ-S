bfe7ff526ee7264a23d7b4d1f0f1d7861330a704
kerkinga <82398616+kerkinga@users.noreply.github.com>
2021-05-08 23:28:24 +0200

    Plusz script a Korreláció számolására és vizualizálásra

---
+++ Script/correlation.R
@@ -0,0 +1,62 @@
+library("rstudioapi")                                 # Load rstudioapi package
+library(dplyr)
+library(ggplot2)
+library(here)
+library(magrittr)
+
+
+
+print("Adatelemzés házi feladat - Öngyilkosság és Egyfőre jutó GDP kapcsolata 1991-2016-ig")
+print("Öngyilkossági adatokat tartalmazó CSV fájl beolvasása.")
+
+#TODO: File beolvasas kiszervezese fuggvenybe
+data <- read.csv(here("Data", "ongyilkossag.csv"),sep=",", header = FALSE) 
+
+print("Öngyilkossági adatokat tartalmazó CSV fájl sikeresen beolvasva.")
+cat("Beolvasott öngyilkossági adatok száma:", nrow(data), "darab sor")
+
+#Beolvasott adatok oszlopainak elnevezése
+names(data) <- c('country','year','sex','age','suicides_no','population','suicides/100k pop','country-year','HDI for year',' gdp_for_year ($) ','gdp_per_capita','generation')
+
+#Szűrés a magyar adatokra.
+hunData <- subset( data, country == "Hungary")
+
+
+hunCor <-hunData %>%
+  group_by(year, gdp_per_capita) %>% 
+  summarise(suicides_no = sum(as.integer(suicides_no)))
+
+# Pearson korreláció számolás
+x<- cor(hun2016ChartData$suicides_no, as.numeric(hun2016ChartData$gdp_per_capita))
+
+print(paste("Korreláció: ", x))
+
+if(x == 0){
+  print("nincs lineáris kapcsolat")
+} else if (-0.2 < x & x < 0 | 0 < x & x <0.2) {
+  print("gyenge, majdnem hanyagolható kapcsolat")
+} else if (-0.4 < x & x < -0.2 | 0.2 < x & x < 0.4) {
+  print("biztos, de gyenge kapcsolat")
+} else if (-0.7 < x & x < -0.4 | 0.4 < x & x < 0.7) {
+  print("közepes korreláció, jelentős kapcsolat")
+} else if (-0.9 < x & x < -0.7 | 0.7 < x & x < 0.9) {
+  print("magas korreláció, markáns kapcsolat") 
+} else if (-1 < x & x < -0.9 | 0.9 < x & x <= 1) {
+  print("nagyon magas korreláció, erős függő kapcsolat") 
+  
+  
+} else
+  print("Hibás")
+
+#Korreláció vizuálisan:
+png(file = here("Output/Chart", "CorrelationBetweenSuicideNoAndGDPPerCapita.png"))
+
+#Scatterplot
+
+plot(suicides_no,gdp_per_capita , main="Korreláció az öngyilkosságok száma és az egyfőre jutó GDP között ",
+     xlab="Öngyilkosságok száma évente (fő)", ylab="Egyfőre jutó GDP ($) ", pch=19)
+# Add fit lines
+abline(lm(gdp_per_capita~suicides_no), col="red") # regression line (y~x)
+lines(lowess(suicides_no,gdp_per_capita), col="blue") # lowess line (x,y)
+
+dev.off()
--- Script/suicide_hungary_2016.R
+++ Script/suicide_hungary_2016.R
@@ -6,7 +6,7 @@ library(magrittr)
 
 
 
-print("Adatelemzés házi feladat - Öngyilkosság és munkanélküliség kapcsolata 1990-2014-ig")
+print("Adatelemzés házi feladat - Öngyilkosság és Egyfőre jutó GDP kapcsolata 1991-2016-ig")
 print("Öngyilkossági adatokat tartalmazó CSV fájl beolvasása.")
 
 #TODO: File beolvasas kiszervezese fuggvenybe
@@ -18,65 +18,40 @@ cat("Beolvasott öngyilkossági adatok száma:", nrow(data), "darab sor")
 #Beolvasott adatok oszlopainak elnevezése
 names(data) <- c('country','year','sex','age','suicides_no','population','suicides/100k pop','country-year','HDI for year',' gdp_for_year ($) ','gdp_per_capita','generation')
 
-#Szűrés a magyar 2016 adatokra.
+#Szűrés a magyar adatokra.
 hunData <- subset( data, country == "Hungary")
 #hun2016Data <- subset(hunData, year <="2016" & year>="1991",)
 
-#szűrésGDP-re
-#hunGDPData <- subset( data, country == "Hungary")
-#hunGDP2016 <-subset(hunGDPData,year <="2016" & year>="2010",)
-#hunGDP2016group <-hunGDP2016 %>% 
- # group_by(year, gdp_per_capita) %>%
 
 #Csoportosítás példa
 hun2016ChartData <- hunData %>% 
+  group_by(year, gdp_per_capita) %>% 
+  summarise(suicides_no = sum(as.integer(suicides_no)))
+
+hunCor <-hunData %>%
   group_by(year, gdp_per_capita) %>% 
   summarise(suicides_no = sum(as.integer(suicides_no)))
   
   
 
 #Ábra képfájl elnevezése
-png(file = here("Output/Chart", "SuicideHungary2010-2016withGDPpercapita.png"))
-
-
-
-
-
-#próba line chart
-#ggplot(hun2016PieChartData, aes(x = year, y = gdp_per_capita)) +                            
-  #geom_line(size = 1.5, color="red", group = 1)
-
-
+png(file = here("Output/Chart", "SuicideHungary1991-2016withGDPpercapita.png"))
 
 
 # EZAJÓchart
 ggplot(data=hun2016ChartData, aes(x=year, y=suicides_no), ) +
+  geom_col(width = 0.5) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  #scale_x_discrete(guide = guide_axis(check.overlap = TRUE)) +
 geom_bar(stat="identity", fill="steelblue") +
-geom_text(aes(label=suicides_no), vjust=-0.3, size=3.5)+
+geom_text(aes(label=suicides_no), vjust=0.1, size=3.2, angle=45, hjust = 0)+ 
+   
 geom_line(aes(x = year, y =0.2*as.numeric(gdp_per_capita)), size = 0.5, color="red", group = 1)+
 #geom_text(aes(label=gdp_per_capita, x=year, y=0.21*as.numeric(gdp_per_capita)), colour="black")+
  scale_y_continuous(sec.axis = sec_axis(~.*5, name = "gdp per capita"))
 
-x<- cor(hun2016ChartData$suicides_no, as.numeric(hun2016ChartData$gdp_per_capita))
-
-if(x == 0){
-  print("nincs lineáris kapcsolat")
-} else if (-0.2 < x & x < 0 | 0 < x & x <0.2) {
-  print("gyenge, majdnem hanyagolható kapcsolat")
-} else if (-0.4 < x & x <-0.2 | 0.2<x<0.4) {
-  print("biztos, de gyenge kapcsolat")
-} else if (-0.7<x & x<-0.4 | 0.4<x & x<0.7) {
-  print("közepes korreláció, jelentős kapcsolat")
-} else if (-0.9<x & x<-0.7 | 0.7<x & x<0.9) {
-  print("magas korreláció, markáns kapcsolat") 
-} else if (-1<x & x<-0.9 | 0.9<x & x<1) {
-  print("nagyon magas korreláció, erős függő kapcsolat") 
-  
-  
-  } else
-  print("Nem jó")
-
 
 
 dev.off()
+
 
