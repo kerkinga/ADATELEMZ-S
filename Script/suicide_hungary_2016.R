70c9d128c1643288e9b50218e412e8f1f882030a
kerkinga <82398616+kerkinga@users.noreply.github.com>
2021-05-08 14:30:15 +0200

    adattisztítás, adatszűrés 1991-2016, barplot és line chart

--- Script/suicide_hungary_2016.R
+++ Script/suicide_hungary_2016.R
@@ -2,6 +2,9 @@ library("rstudioapi")                                 # Load rstudioapi package
 library(dplyr)
 library(ggplot2)
 library(here)
+library(magrittr)
+
+
 
 print("Adatelemzés házi feladat - Öngyilkosság és munkanélküliség kapcsolata 1990-2014-ig")
 print("Öngyilkossági adatokat tartalmazó CSV fájl beolvasása.")
@@ -13,25 +16,48 @@ print("Öngyilkossági adatokat tartalmazó CSV fájl sikeresen beolvasva.")
 cat("Beolvasott öngyilkossági adatok száma:", nrow(data), "darab sor")
 
 #Beolvasott adatok oszlopainak elnevezése
-names(data) <- c('country','year','sex','age','suicides_no','population','suicides/100k pop','country-year','HDI for year',' gdp_for_year ($) ','gdp_per_capita ($)','generation')
+names(data) <- c('country','year','sex','age','suicides_no','population','suicides/100k pop','country-year','HDI for year',' gdp_for_year ($) ','gdp_per_capita','generation')
 
 #Szűrés a magyar 2016 adatokra.
 hunData <- subset( data, country == "Hungary")
-hun2016Data <- subset(hunData, year == "2016")
+#hun2016Data <- subset(hunData, year <="2016" & year>="1991",)
+
+#szűrésGDP-re
+#hunGDPData <- subset( data, country == "Hungary")
+#hunGDP2016 <-subset(hunGDPData,year <="2016" & year>="2010",)
+#hunGDP2016group <-hunGDP2016 %>% 
+ # group_by(year, gdp_per_capita) %>%
 
 #Csoportosítás példa
-hun2016PieChartData <- hun2016Data %>% 
-  group_by(age) %>% 
+hun2016ChartData <- hunData %>% 
+  group_by(year, gdp_per_capita) %>% 
   summarise(suicides_no = sum(as.integer(suicides_no)))
+  
+  
 
 #Ábra képfájl elnevezése
-png(file = here("Output/Chart", "SuicideHungary2016ByAges.png"))
+png(file = here("Output/Chart", "SuicideHungary2010-2016withGDPpercapita.png"))
 
-#Ábra generálása.
-pie(hun2016PieChartData$suicides_no, 
-    labels = paste(hun2016PieChartData$age, sep = ":", hun2016PieChartData$suicides_no),
-    col = rainbow(length(hun2016PieChartData$suicides_no)), 
-    main = "2016-s Magyrországi öngyilkosságok száma korosztályonként"
-    )
+
+
+
+
+#próba line chart
+#ggplot(hun2016PieChartData, aes(x = year, y = gdp_per_capita)) +                            
+  #geom_line(size = 1.5, color="red", group = 1)
+
+
+
+
+# EZAJÓchart
+ggplot(data=hun2016ChartData, aes(x=year, y=suicides_no), ) +
+geom_bar(stat="identity", fill="steelblue") +
+geom_text(aes(label=suicides_no), vjust=-0.3, size=3.5)+
+geom_line(aes(x = year, y =0.2*as.numeric(gdp_per_capita)), size = 0.5, color="red", group = 1)+
+#geom_text(aes(label=gdp_per_capita, x=year, y=0.21*as.numeric(gdp_per_capita)), colour="black")+
+ scale_y_continuous(sec.axis = sec_axis(~.*5, name = "gdp per capita"))
+
+
 
 dev.off()
+
